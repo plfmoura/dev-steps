ad952d42b60a39357d10790037e9f7dca8b26411
plfmoura <plfmoura96@gmail.com>
2023-10-04 02:02:11 -0300

    feat: add primevue ui lib

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import './assets/main.css'
 
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(PrimeVue);
 app.use(router)
 
 app.mount('#app')
