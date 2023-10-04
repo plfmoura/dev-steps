44133f21a636953b7a49ef2b9d9235459caa8184
plfmoura <plfmoura96@gmail.com>
2023-10-04 01:24:45 -0300

    refactor: delete all unused files

---
+++ src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<template >
+     <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+</template>
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+    components: {
+        RouterLink
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-  </main>
+    <h1>Welcome to Dev Steps</h1>
+    <h2>Leave one piece of advice you wish you had heard from your present self?</h2>
+</main>
 </template>
